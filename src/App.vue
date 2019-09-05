3c09fee92be670783ed9f34911bb98b58ab0e5d0
lujinggirl <lujing15@jd.com;lujing_girl@163.com>
2019-09-05 17:14:54 +0800

    vue-router&vuex

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  },
-  created () {
-    this.getLoginInfo();
-  },
-  methods: {
-    getLoginInfo () {
-      console.log(22)
-      this.$api.getLoginInfo().then((response) => {
-        console.log(22, response)
-        if (response.errno === 10000) {
-          console.log(22, response)
-          if (response.data.status === 'loggedOut') {
-            // 当前是登出状态，需要跳到erp去登陆。
-            const currenHref = window.location.href;
-            window.location.href = this.Config.LOGIN_URL + window.location.href;
-          } else if (response.data.status === 'loggedIn') {
-            this.getFilterTicket();
-            this.getListPermissions();
-            this.$store.commit('GET_USER', response.data);
-            this.$store.commit('GET_ADMIN', response.data.isAdmin);
-          }
-        }
-      });
-    },
-  }
-}
-</script>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue'
 import EchojoyUI from "echojoy-ui";
 import "echojoy-ui/lib/echojoy-ui.css";
 import api from "@/request/index";
+import router from './router'
+import store from './store'
 Vue.use(EchojoyUI);
 Vue.config.productionTip = false
 
 
 Vue.use(api);
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
